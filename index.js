4bddbee80bc75cc97c83b86b00f9fc2d5f9ee8ce
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-21 11:12:50 +0100

    xd

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.get('./home', (req, res) => {
+  res.send({
+    data: "Hello world"
+  })
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
